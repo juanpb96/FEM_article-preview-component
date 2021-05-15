5a39fc4f260a804eba526abd20de5af7c001e61a
MasterDev <juanpb.96@gmail.com>
2021-05-15 14:44:14 -0500

    Add script file

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggle = () => {
+    const element = 'profile';
+
+    if (profileSection.className.includes('--active')) {
+        profileSection.className = element;
+        authorDiv.className = `${element}__author`;
+        socialDiv.className = ` ${element}__social`;
+    } else {
+        profileSection.className += ` ${element}--active`;
+        authorDiv.className += ` ${element}__author--hidden`;
+        socialDiv.className += ` ${element}__social--active`;
+    }
+
+};
+
+const profileSection = document.getElementsByClassName('profile')[0];
+const authorDiv = document.getElementsByClassName('profile__author')[0];
+const socialDiv = document.getElementsByClassName('profile__social')[0];
+const buttons = document.getElementsByTagName('button');
+
+[...buttons].forEach(button => {
+    button.addEventListener('click', toggle);    
+});
